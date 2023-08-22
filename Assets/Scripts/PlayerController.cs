2cdcc49fee9b56565854f8b2c089f740983fdff7
edson152 <71998101+edson152@users.noreply.github.com>
2023-08-23 05:28:17 +0800

    first comit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountTest();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("PickUp")){
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountTest();
+		}
+	}
+
+	void SetCountTest(){
+		countText.text = "Count: " + count.ToString();
+		if (count >= 8) {
+			winText.text = "You win!";
+		}
+	}
+
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Rotater : MonoBehaviour {
+
+	private Vector3 rotation = new Vector3(
+		15, 30, 45
+	);
+
+	void Update () {
+		transform.Rotate(rotation * Time.deltaTime);
+	}
+}
